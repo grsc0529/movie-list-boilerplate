3dc862cb1276c16013de8870b82595ca70723fb7
Greta Schock <gschockcodes@gmail.com>
2021-02-13 16:02:34 -0800

    Added request methods to index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,53 @@
-const express = require('express');
-const app = express();
+//What tools do we need to create our server connection?
+const express = require('express'); //express is a node library
+const path = require('path'); //path is a way to create easy paths to the files in your repo
+const db = require('../db'); //import sequel connection
+
+//Estanlish connection to PORT and directory to /public files 
 const PORT = 3000 || process.env.PORT;
-const db = require('../db');
+const PUBLIC_DIR = path.resolve(__dirname, '..', 'client', 'dist');
+const app = express(); //invoke express
 
+//create middleware 
+const loggingMiddleware = (res, req, next) => {
+  var logStr = `${req.method} request coming from ${req.url}`;
+  console.log(logStr);
+  next();
+}
 
-app.use(express.static('public'));
+//tell the app what it is going to use to create connections
+app.use(loggingMiddleware);
+app.use(express.json());
+app.use(express.static(PUBLIC_DIR));
+
+//construct request methods 
+
+app.get('/api/movieTable', (req, res) => {
+  const sql = `SELECT * FROM movieTable`;
+  db.query(sql, (err, movieData) => {
+    if (err) {
+      console.log(err);
+      res.send(500);
+    } else {
+      res.send(movieData);
+    }
+  })
+});
+
+app.post('/api/movieTable', (req, res) => {
+  const { movieTable, watched, releaseYear, metascore, IMBDrating } = req.body;
+  const sql = `
+    INSERT INTO movieTable (movieTable, watched, releaseYear, metascore, IMBDrating) VALUES (?, ?, ?, ?, ?)
+    `;
+    db.query(sql, [movieTable, watched, releaseYear, metascore, IMBDrating], (err, data) => {
+      if (err) {
+        console.log(err);
+        res.send(500);
+      } else {
+        res.send(201);
+      }
+    })
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
