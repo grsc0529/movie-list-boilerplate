45dd79599bac7d1159c4271e2fdf7b9376c1259b
Greta Schock <gschockcodes@gmail.com>
2021-02-17 12:32:14 -0800

    Added the panel feature to movie Info

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 class MovieList extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      isShowingMovieInfo: false
+    }
     
     //Bind Methods
     this.renderWatchedLabel = this.renderWatchedLabel.bind(this);
     this.handleWatchedButtonClick = this.handleWatchedButtonClick.bind(this);
-
+    this.handleMovieTitleClick = this.handleMovieTitleClick.bind(this);
   }
 
   renderWatchedLabel(boolean) {
@@ -21,6 +25,9 @@ class MovieList extends React.Component {
   handleMovieTitleClick(event) {
     event.preventDefault();
     console.log('Movie Title Was Clicked');
+    var antiCurrentState = !this.state.isShowingMovieInfo;
+    this.setState({isShowingMovieInfo: antiCurrentState});
+    console.log(this.state.isShowingMovieInfo)
   }
 
   handleWatchedButtonClick(event) {
@@ -31,12 +38,23 @@ class MovieList extends React.Component {
   }
 
   render() {
+    if (this.state.isShowingMovieInfo) {
+      var additionalMovieInfo =
+          <tr>
+            <td>Release Year: <em>{this.props.movieObj.releaseYear}</em></td>
+            <td>Metascore: <em>{this.props.movieObj.metascore}</em></td>
+            <td>IMBD Rating: <em>{this.props.movieObj.IMBDrating}</em></td>
+            <td>PERSONAL RATING</td>
+            <td><button onClick={this.handleWatchedButtonClick}>{this.renderWatchedLabel(this.props.movieObj.watched)}</button></td>
+          </tr>
+    }
+
     return (
       <tbody>
       <tr>
         <td onClick={this.handleMovieTitleClick}>{this.props.movieObj.movieTitle}</td>
-        <td><button onClick={this.handleWatchedButtonClick}>{this.renderWatchedLabel(this.props.movieObj.watched)}</button></td>
       </tr>
+      {additionalMovieInfo}
       </tbody>
     )
   }
